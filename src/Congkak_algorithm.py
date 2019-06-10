7c012c9cbb325e5c5ae11d916c626de03dc8b4c1
Ang56187 <ax54@live.com>
2019-06-10 23:01:48 +0800

    Edit to board or somethin

--- src/Congkak_algorithm.py
+++ src/Congkak_algorithm.py
@@ -6,25 +6,30 @@ class Congkak_algorithm:
         self.algorithm()
 
     def algorithm(self):
-        self.board = np.zeros((2,8),dtype=int)
-        self.board[:,0:7] = 0
-        self.current_player=0
+        self.board = np.zeros((2,7),dtype=int)
+        self.board[:,0:7] = 7
+        self.current_player = 1
 
     def print_board(self):
         print(self.board)
 
-    def check_end(self):
+    def check_empty(self):
         ##check if game have ended or not
-        ##if all holes were emptied out
-        
-        for i in range(len(self.board)):
-            for j in  range(len(self.board[i])):
-                if self.board[i][j] >0:
-                    print("Yes")
-                    return False
-        print("No")
+        ##if one side of hole were emptied out
 
-    def play_move(self,i,j):
+        for j in range(len(self.board[0])):
+            if self.board[0][j] >0:
+                print("Not empty on player side")
+                return True
+            elif self.board[1][j] > 0:
+                print("Not empty on apponent side")
+                return True
+
+    def spill_seed(self,col,row):
+        ##add seeds for each hole
+        self.board[col][row-1]+=1
+
+    def move(self,i,j):
         if self.board[0,7] + self.board[1,7] == 98:
             return True
         return False
@@ -34,14 +39,49 @@ class Congkak_algorithm:
             return True
         return False
 
+    #where the game starts playing
     def play_game(self):
-        self.check_end()
+        while self.check_empty():
+            self.print_board()
 
-            ##self.print_board()
-            ##inputMove = int(input("Enter 0-7"))
-            ##if inputMove>=0 and inputMove<7:
-                ##print("SURE")
-            ##else:
-                ##print("Only 0-7 allowed")
+            ## allow player to choose where to place shells
+            inputMove = int(input("Enter 0-6:"))
+            while not 0<=inputMove<7:
+                    inputMove = int(input("Please enter 0-6:"))
+
+            while self.board[0][inputMove] == 0:
+                inputMove = int(input("Please select valid and non-zero hole:"))
+            print("Input in")
+
+
+            ## create copy so before shells in hole got emptied, and shells grabbed into hand
+            no_times_loop = self.board[0,inputMove]
+
+            ## empty out the selected hole
+            self.board[0,inputMove]=0
+
+            col = 0
+
+            ## start with players first
+            ## iterate no of times based on shells on selected hole
+            for moves in range(no_times_loop,-1,-1):
+                print("column:",col)
+
+                if inputMove<=0:
+                    col=1
+
+
+                if col == 0:
+                    self.print_board()
+                    self.spill_seed(col,inputMove)
+                    print(inputMove)
+                    inputMove -= 1
+                elif col == 1:
+                    self.print_board()
+                    self.spill_seed(col,inputMove)
+                    print(inputMove)
+                    inputMove+=1
+
+
 
 
